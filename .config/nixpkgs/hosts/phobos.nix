8c025fdd259f70852d23deeb542aba791893d6ff
John Axel Eriksson <john@insane.se>
2018-08-24 19:55:56 +0200

    Add phobos host config

---
+++ .config/nixpkgs/hosts/phobos.nix
@@ -0,0 +1,13 @@
+{
+  alacritty = {
+    fontSize = "24.0";
+    largeFontSize = "40.0";
+  };
+  xresources = {
+    dpi = "196";
+    rofiFont = "Roboto 36";
+  };
+  i3 = rec {
+    fontSize = "14";
+  };
+}
