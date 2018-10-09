9f51433c4df0ee1f97cf66de4674aeaa9007584b
John Axel Eriksson <john@insane.se>
2018-10-09 23:19:32 +0200

    Update phobos config

--- .config/nixpkgs/hosts/phobos.nix
+++ .config/nixpkgs/hosts/phobos.nix
@@ -1,16 +1,7 @@
 {
-  alacritty = {
-    fontSize = "24.0";
-    largeFontSize = "40.0";
-  };
-  xresources = {
-    dpi = "196";
-    rofiFont = "Roboto 36";
-  };
   i3 = rec {
-    fontSize = "14";
     sway-outputs = {
-      eDP-1 = "scale 2 pos 0 0";
+      eDP-1 = "scale 2 pos 0 0 res 3200x1800";
     };
   };
 }
