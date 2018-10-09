834d637a26bd6abd5d46cc74bf545ab14ab55b6d
John Axel Eriksson <john@insane.se>
2018-10-09 19:48:54 +0200

    Fix deprecation warning in my-emacs package

--- .config/nixpkgs/packages/my-emacs/default.nix
+++ .config/nixpkgs/packages/my-emacs/default.nix
@@ -11,7 +11,7 @@ let
   '';
 
   emacsPackages =
-    pkgs.emacsPackagesNg.overrideScope
+    pkgs.emacsPackagesNg.overrideScope'
     (self: super: {
       inherit (self.melpaPackages)
         evil flycheck-haskell haskell-mode
