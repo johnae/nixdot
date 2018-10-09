dad1b9743f6f64ed7381a481d79af575464a6abb
John Axel Eriksson <john@insane.se>
2018-10-09 19:45:01 +0200

    Remove dropbox as they don't want to support linux anymore

--- .config/nixpkgs/overlays/home.nix
+++ .config/nixpkgs/overlays/home.nix
@@ -5,7 +5,6 @@ self: super: {
           dotfiles
           my-emacs
           latest.firefox-beta-bin
-          dropbox
           signal-desktop
           awscli
           my-google-cloud-sdk
