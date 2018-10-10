3e1cccdb6bdba7291a31c24075603330e11376ef
John Axel Eriksson <john@insane.se>
2018-10-10 16:35:29 +0200

    Update persway

--- .config/nixpkgs/packages/persway/default.nix
+++ .config/nixpkgs/packages/persway/default.nix
@@ -3,14 +3,14 @@
 buildGoPackage rec {
   name = "persway-unstable-${version}";
   version = "2018-10-08";
-  rev = "4e690623e69dba03293d7edfe7e45bbf4fe9440c";
+  rev = "4edc21dd1c776521c64fb3d5b1d09a2f5ef43766";
 
   goPackagePath = "github.com/johnae/persway";
 
   src = fetchgit {
     inherit rev;
     url = "https://github.com/johnae/persway";
-    sha256 = "1spddqc3vqrf7zd33kngaxaf10wfibvw7rh2cinz8lssibi7nwmh";
+    sha256 = "074kh2n09vjcljczf9p8d1yqza8rd65f9r51w7a2hk4j6wbyav6m";
   };
 
   goDeps = ./deps.nix;
