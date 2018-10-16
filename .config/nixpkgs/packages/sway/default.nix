4b596c6ee3ec6e502b0265877fbeda91fdcdbd0e
John Axel Eriksson <john@insane.se>
2018-10-16 12:43:04 +0200

    Update sway

--- .config/nixpkgs/packages/sway/default.nix
+++ .config/nixpkgs/packages/sway/default.nix
@@ -1,19 +1,21 @@
-{ stdenv, fetchFromGitHub, meson, ninja, pkgconfig, asciidoc, libxslt, docbook_xsl, scdoc
-, wayland, wayland-protocols, xwayland, libxkbcommon, pcre, json_c, dbus_libs
-, pango, cairo, libinput, libcap, pam, gdk_pixbuf, libpthreadstubs
+{ stdenv, fetchFromGitHub, meson, ninja, pkgconfig
+, asciidoc, libxslt, docbook_xsl, scdoc
+, wayland, wayland-protocols, xwayland, libxkbcommon
+, pcre, json_c, dbus_libs, pango, cairo, libinput
+, libcap, pam, gdk_pixbuf, libpthreadstubs
 , libXdmcp, wlroots, git, systemd, wrapGAppsHook
 , buildDocs ? true
 }:
 
 stdenv.mkDerivation rec {
   name = "sway-${version}";
-  version = "cd02d60a992ee38689a0d17fc69c4e2b1956f266";
+  version = "113751ea48b9ffa537c357373913ca9caecb68e0";
 
   src = fetchFromGitHub {
     owner = "swaywm";
     repo = "sway";
     rev = version;
-    sha256 = "0fxbqg9b7k428krbhsvnbq8kmaxg4c07yin6n2r6aacx2v5wpa2k";
+    sha256 = "1m0jwdcjk5v66i2cdhhpfm2gscl6kbxnw83bzgqiys3b3m6lvjm3";
   };
 
   nativeBuildInputs = [
