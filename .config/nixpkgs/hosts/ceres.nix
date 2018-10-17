abe44c2aad75734e6145baaf5f693012baff80f9
John Axel Eriksson <john@insane.se>
2018-10-17 15:00:34 +0200

    Add ceres host config. Add proper 24bit terminfo.

---
+++ .config/nixpkgs/hosts/ceres.nix
@@ -0,0 +1,19 @@
+{
+  WINIT_HIDPI_FACTOR = "2.0";
+  sway = rec {
+    sway-outputs = {
+      eDP-1 = "scale 2.0 pos 0 0 res 3840x2160";
+    };
+    sway-inputs = {
+      "1739:30383:DELL07E6:00_06CB:76AF_Touchpad" = {
+        dwt = true;
+        tap = true;
+        natural_scroll = true;
+      };
+      "1118:2354:Surface_Arc_Mouse_Keyboard" = {
+        dwt = true;
+        natural_scroll = true;
+      };
+    };
+  };
+}
