39292a059f0efc973fde7063041401e05c0b7c6a
John Axel Eriksson <john@insane.se>
2019-01-04 23:52:58 +0100

    Update phobos touchpad config

--- .config/nixpkgs/hosts/phobos.nix
+++ .config/nixpkgs/hosts/phobos.nix
@@ -5,5 +5,16 @@
     sway-outputs = {
       eDP-1 = "scale 2.0 pos 0 0 res 3200x1800";
     };
+    sway-inputs = {
+      "1739:30383:DLL075B:01_06CB:76AF_Touchpad" = {
+        dwt = true;
+        tap = true;
+        natural_scroll = true;
+      };
+      "1118:2354:Surface_Arc_Mouse_Keyboard" = {
+        dwt = true;
+        natural_scroll = true;
+      };
+    };
   };
 }
