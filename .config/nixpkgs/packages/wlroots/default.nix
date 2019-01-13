238344e2947da40d8c3ccdaeaa513bba1fec3ca0
John Axel Eriksson <john@insane.se>
2019-01-13 22:36:49 +0100

    update wlroots

--- .config/nixpkgs/packages/wlroots/default.nix
+++ .config/nixpkgs/packages/wlroots/default.nix
@@ -9,8 +9,8 @@ stdenv.mkDerivation rec {
   src = fetchFromGitHub {
     owner = "swaywm";
     repo = "wlroots";
-    rev = "8134d5b1e0b8c65f7c8b2e7ae626c1f2e5cbc9e1";
-    sha256 = "0xkm8lcsbp142y66n4b3wbs9xrdcnapsnydi7ykawjy933ms6pz9";
+    rev = "10a2c4edec5e4f0877ff4afc83178c3f08b4f063";
+    sha256 = "11nrnc8jia00k66iw1rwzdvpb4pd0hmpnlqavwryqxdia8kg404s";
   };
 
   # $out for the library and $bin for rootston
