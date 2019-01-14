e41fa90ec4b85aa0249eb44e82ed1e8828351c37
John Axel Eriksson <john@insane.se>
2019-01-14 15:53:39 +0100

    Update spook

--- .config/nixpkgs/packages/spook/default.nix
+++ .config/nixpkgs/packages/spook/default.nix
@@ -1,14 +1,14 @@
 { stdenv, fetchgit, gnumake, gcc, wget, perl, cacert }:
 
 stdenv.mkDerivation rec {
-  version = "0.9.6-pre";
+  version = "0.9.6";
   name = "spook-${version}";
   SPOOK_VERSION = version;
 
   src = fetchgit {
     url = https://github.com/johnae/spook.git;
-    rev = "10c817ecef6cc57ad36f66930eed167da512fed7";
-    sha256 = "05ix72zix48snn6dc76mvxycb1q96z0bbc1k8hw1kbp4r0jc77f4";
+    rev = "0.9.6";
+    sha256 = "12k7n0g1mbaaf0wnbhdvmyn18kwidd24faz50w0b6akxlds85ml1";
     fetchSubmodules = true;
   };
 
