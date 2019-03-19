e912c8b3811e226671be2d27fbe0b0f989ff3184
John Axel Eriksson <john@insane.se>
2019-03-19 16:38:08 +0100

    Update ceres config

--- .config/nixpkgs/hosts/ceres.nix
+++ .config/nixpkgs/hosts/ceres.nix
@@ -1,20 +1,29 @@
 {stdenv, lib, pkgs, ...}:
 
+let
+
+  mouse = {
+    dwt = true;
+    natural_scroll = true;
+  };
+
+  touchpad = {
+    dwt = true;
+    tap = true;
+    natural_scroll = true;
+  };
+
+in
+
 {
   sway = {
     sway-outputs = {
       eDP-1 = "scale 2.0 pos 0 0 res 3840x2160";
+      "\"Unknown ASUS PB27U 0x0000C167\"" = "scale 1.6";
     };
     sway-inputs = {
-      "1739:30383:DELL07E6:00_06CB:76AF_Touchpad" = {
-        dwt = true;
-        tap = true;
-        natural_scroll = true;
-      };
-      "1118:2354:Surface_Arc_Mouse_Keyboard" = {
-        dwt = true;
-        natural_scroll = true;
-      };
+      "1739:30383:DELL07E6:00_06CB:76AF_Touchpad" = touchpad;
+      "1118:2354:Surface_Arc_Mouse_Keyboard" = mouse;
     };
   };
 }
