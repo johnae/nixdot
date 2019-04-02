73f73e686457e096942e5002ac05dbddfcd8be3a
John Axel Eriksson <john@insane.se>
2019-04-02 22:44:38 +0200

    Lock mozilla overlay to specific commit

--- .config/nixpkgs/overlays/mozilla.nix
+++ .config/nixpkgs/overlays/mozilla.nix
@@ -1,1 +1,5 @@
-import (builtins.fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz)
+import (builtins.fetchGit {
+    url = "https://github.com/mozilla/nixpkgs-mozilla.git";
+    ref = "master";
+    rev = "50bae918794d3c283aeb335b209efd71e75e3954";
+})
