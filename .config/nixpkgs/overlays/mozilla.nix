9b91bb745ac62e986ca55b39b944bd292a211656
John Axel Eriksson <john@insane.se>
2019-05-11 09:40:35 +0200

    Make mozilla nixpkgs easier to update

--- .config/nixpkgs/overlays/mozilla.nix
+++ .config/nixpkgs/overlays/mozilla.nix
@@ -1,5 +1,11 @@
-import (builtins.fetchGit {
-    url = "https://github.com/mozilla/nixpkgs-mozilla.git";
-    ref = "master";
-    rev = "50bae918794d3c283aeb335b209efd71e75e3954";
-})
+let
+  meta = builtins.fromJSON (builtins.readFile ./mozilla.json);
+in
+  import (builtins.fetchGit {
+    inherit (meta) url rev;
+  })
+#import (builtins.fetchGit {
+#    url = "https://github.com/mozilla/nixpkgs-mozilla.git";
+#    ref = "master";
+#    rev = "50bae918794d3c283aeb335b209efd71e75e3954";
+#})
