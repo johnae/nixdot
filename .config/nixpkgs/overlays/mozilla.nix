2b03c0bfd4e7473d0d2f852e42c1bd04da11158c
John Axel Eriksson <john@insane.se>
2019-06-13 23:18:30 +0200

    Update mozilla nixpkgs

--- .config/nixpkgs/overlays/mozilla.nix
+++ .config/nixpkgs/overlays/mozilla.nix
@@ -4,8 +4,3 @@ in
   import (builtins.fetchGit {
     inherit (meta) url rev;
   })
-#import (builtins.fetchGit {
-#    url = "https://github.com/mozilla/nixpkgs-mozilla.git";
-#    ref = "master";
-#    rev = "50bae918794d3c283aeb335b209efd71e75e3954";
-#})
