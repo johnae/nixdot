0331caf8fa02981b5e2e8618130958e76c1c19df
John Axel Eriksson <john@insane.se>
2019-11-21 11:34:01 +0100

    Increase max results pages from spotnix

--- .config/nixpkgs/hosts/defaults.nix
+++ .config/nixpkgs/hosts/defaults.nix
@@ -118,7 +118,7 @@ rec {
       };
       Service = {
          ExecStart = ''
-           ${stdenv.shell} -c 'CLIENT_ID="$(${pkgs.pass}/bin/pass web/spotify.com/spotnix | head -1)" CLIENT_SECRET="$(${pkgs.pass}/bin/pass web/spotify.com/spotnix | tail -1)" REDIRECT_URI="http://localhost:8182/spotnix" ${pkgs.spotnix}/bin/spotnix -d ${meta.hostName} -e $XDG_RUNTIME_DIR/spotnix_event -i $XDG_RUNTIME_DIR/spotnix_input -o $XDG_RUNTIME_DIR/spotnix_output'
+           ${stdenv.shell} -c 'CLIENT_ID="$(${pkgs.pass}/bin/pass web/spotify.com/spotnix | head -1)" CLIENT_SECRET="$(${pkgs.pass}/bin/pass web/spotify.com/spotnix | tail -1)" REDIRECT_URI="http://localhost:8182/spotnix" ${pkgs.spotnix}/bin/spotnix -d ${meta.hostName} -e $XDG_RUNTIME_DIR/spotnix_event -i $XDG_RUNTIME_DIR/spotnix_input -o $XDG_RUNTIME_DIR/spotnix_output -r 10'
          '';
          Restart = "always";
          RestartSec = 3;
